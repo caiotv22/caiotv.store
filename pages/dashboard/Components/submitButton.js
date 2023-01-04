f5c273d9dcf057b9c10f3e0c6a2b08d86b061e27
Uriel <urieligara@hotmail.com>
2023-01-04 02:27:37 -0300

    SubmitButton

---
+++ pages/dashboard/Components/submitButton.js
@@ -0,0 +1,7 @@
+export default function SubmitButton() {
+  return (
+    <button className='bg-defaultGreen hover:bg-emerald-700 text-white py-2 px-4 rounded' type='submit'>
+      Salvar Alterações
+    </button>
+  );
+}
