677e64124ffebc2eee16c00ff3721e21abeb4074
Uriel <urieligara@hotmail.com>
2023-01-04 02:35:56 -0300

    Input added

--- pages/dashboard/MenuOptions/banner.js
+++ pages/dashboard/MenuOptions/banner.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import Input from '@/dashboard/components/input';
 
-export default function Banner() {
+export default function Banner({ data }) {
+  const dataToObj = JSON.parse(data);
   return (
     <>
-      <h1>Banner</h1>
+      <h1 className='mb-4'>Banner</h1>
+      <Input
+        id='option_1'
+        labelTitle={'Texto Principal'}
+        setInput={'setGetInputMenu1'}
+        defaultVal={dataToObj?.translation?.hero_section?.title}
+        placeHolderVal='Digite aqui o título da nova opção do menu'
+        maxLengthVal='30'
+      />
     </>
   );
 }
