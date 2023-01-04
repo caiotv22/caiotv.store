b3574753312eb4ab35f93acd96159e9f2f718c82
Uriel <urieligara@hotmail.com>
2023-01-04 02:29:12 -0300

    MenuOptions

---
+++ pages/dashboard/MenuOptions/plans.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Plans() {
+  return (
+    <>
+      <h1>Plans</h1>
+    </>
+  );
+}
---
+++ pages/dashboard/MenuOptions/plans2.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Plans2() {
+  return (
+    <>
+      <h1>Plans2</h1>
+    </>
+  );
+}
