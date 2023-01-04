3d8fba6bae4db4ec7c3e2e1f8a8fb8a90b6b9dbc
Uriel <urieligara@hotmail.com>
2023-01-04 02:26:36 -0300

    Providers added

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
-// import '../styles/globals.css';
-import '../public/css/main.css';
+import '../styles/globals.css';
+// import '../public/css/main.css';
 import '../i18n';
+import { Providers } from '../global/providers';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Providers>
+      <Component {...pageProps} />
+    </Providers>
+  );
 }
 
 export default MyApp;
