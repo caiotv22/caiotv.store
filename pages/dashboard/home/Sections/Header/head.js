6a03e1288b7cd3c86a5d6c13f113f92f2837189e
Uriel <urieligara@hotmail.com>
2023-01-07 03:03:54 -0300

    head

---
+++ pages/dashboard/home/Sections/Header/head.js
@@ -0,0 +1,64 @@
+import { useState } from 'react';
+import { updateGitHubJson, getGitHubInfo } from '@/services/api/github';
+import Input from '@/dashboard/components/input';
+import SubmitButton from '@/dashboard/components/submitButton';
+import { useGitHubInfo } from '@/global/providers';
+
+export default function Head({ data }) {
+  const dataToObj = data ? JSON.parse(data) : '';
+  const { gitHubUserInfo } = useGitHubInfo();
+  const [getInputTitle, setGetInputTitle] = useState(null);
+  const [getInputDescription, setGetInputDescription] = useState(null);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    let newObj = {};
+    const head = {
+      translation: {
+        head: {
+          title: getInputTitle ? getInputTitle : dataToObj?.translation?.head?.title,
+          description: getInputDescription ? getInputDescription : dataToObj?.translation?.head?.description,
+        },
+      },
+    };
+
+    Object.keys(head).forEach((key) => {
+      if (head[key] instanceof Object) {
+        newObj[key] = Object.assign({}, dataToObj[key], head[key]);
+      } else {
+        newObj[key] = head[key];
+      }
+    });
+    try {
+      let auth = await getGitHubInfo('auth', gitHubUserInfo);
+      console.log(auth);
+      return updateGitHubJson(newObj, auth, 'head updated', gitHubUserInfo);
+    } catch (error) {
+      console.log('erro');
+    }
+  }
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <h2 className='mb-4'>Cabeçalho</h2>
+        <Input
+          id='title'
+          labelTitle={'Título do site'}
+          setInput={setGetInputTitle}
+          defaultVal={dataToObj?.translation?.head?.title}
+          placeHolderVal='Digite aqui o título do seu site...'
+          maxLengthVal='64'
+        />
+        <Input
+          id='description'
+          labelTitle={'Descrição do site'}
+          setInput={setGetInputDescription}
+          defaultVal={dataToObj?.translation?.head?.description}
+          placeHolderVal='Digite aqui a descrição do site...'
+          maxLengthVal='155'
+        />
+        <SubmitButton />
+      </form>
+    </>
+  );
+}
